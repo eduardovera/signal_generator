ff7101fac96a334061961b45c1aeb431a95298e0
Eduardo Vera Sousa <eduardoverasousa@outlook.com>
2015-06-14 21:42:47 -0300

    Added: Signal 03.

---
+++ Signal03.m
@@ -0,0 +1,7 @@
+function [ signal ] = Signal03()
+    N = 1000;
+    signal = zeros(N);
+    for (i = 1:N)
+        signal(i) = exp((-(i-400)^2) / 200) * cos (pi * (i - 400) / 5) + exp((-(i - 425)^2) / 600) * cos((pi * (i - 425)) / 7.2) + exp((-(i - 440)^2) / 400) * cos((pi * (i - 400)) / 5.3);
+    end
+end
